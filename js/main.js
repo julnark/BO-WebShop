1a0786debdd4329c4d40036085cefff322af60d3
Julnar <36888@ma-web.nl>
2024-06-20 21:31:57 +0200

    SearchBarKlaar

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.toggle('dark-mode');
     });
 });
+
+
+//SearchBar//
+function toggleSearch() {
+    const searchContainer = document.getElementById('search-container');
+    searchContainer.classList.toggle('expanded');
+    const searchInput = document.getElementById('search-input');
+    if (searchContainer.classList.contains('expanded')) {
+        searchInput.focus();
+    }
+}
